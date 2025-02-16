630d2d205799e79009d5c3ed607ab79b1cb64d66
VedanshPandey85 <202312085@daiict.ac.in>
2025-02-17 01:42:33 +0530

    "Added Multi slot update using excel "

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,79 +1,13 @@
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const express = require('express')
+const router = express.Router();
+const adminAuth = require('../middleware/adminAuthMiddleware');
+const crypto = require('crypto');
 
-exports.updateRole = async (req, res) => {
-  const { userId, newRole } = req.body;
 
-  try {
-    const user = await User.findById(userId);
-    if (!user) return res.status(404).json({ message: "User not found" });
+router.post('/login',(req,res)=>{
+  const {password} = req.body;
 
-    user.role = newRole;
-    await user.save();
-    res.status(200).json({ message: `User role updated to ${newRole}` });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+  const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
 
-exports.signup = async (req, res) => {
-  const { username, email, password } = req.body;
-
-  try {
-    // Check if the user already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    // Hash the password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    // Create a new user
-    const newUser = new User({
-      username,
-      email,
-      password: hashedPassword,
-      role: "user", // Default role is 'user'
-    });
-
-    // Save the user to the database
-    await newUser.save();
-
-    // Generate a token
-    const token = jwt.sign(
-      { id: newUser._id, role: newUser.role },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-
-    res.status(201).json({ message: "User registered successfully", token });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-exports.login = async (req, res) => {
-  const { username, password } = req.body;
-
-  try {
-    const user = await User.findOne({ username });
-
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid)
-      return res.status(401).json({ message: "Invalid credentials" });
-
-    const token = jwt.sign(
-      { id: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-    res.status(200).json({ token });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+ 
+})
---
+++ middleware/uploadFile.js
@@ -0,0 +1,27 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+// File filter to accept only Excel files
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype ===
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" ||
+    file.mimetype === "application/vnd.ms-excel"
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only Excel files are allowed"), false);
+  }
+};
+
+const upload = multer({ storage, fileFilter });
+
+module.exports = upload;
